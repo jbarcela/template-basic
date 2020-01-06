e96bb51a56f48d961d7e4c713687a550a6dc9e17
Jonathan Barcela <jonathan_rocha_barcela@hotmail.com>
2020-01-05 23:32:26 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx']
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        svg: 'always'
+      }
+    ]
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+};
---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App: React.FC = () => <h1>Template basic</h1>;
+
+export default App;
